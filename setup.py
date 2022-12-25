bd1c99cf2c2627ca6012e1425abb9bfc310c99ff
whh <1757280079@qq.com>
2022-12-25 13:27:14 +0800

    精简

--- setup.py
+++ setup.py
@@ -5,13 +5,9 @@ setup(
     include_package_data=True,
     long_description_content_type="text/markdown",
     install_requires=[
-        "dbus-python==1.2.16",
-        "Flask==1.1.2",
-        "Flask-SocketIO==5.0.1",
+        "dbus-python",
         "eventlet==0.31.0",
-        "blessed==1.17.10",
         "pynput==1.7.1",
-        "psutil==5.6.6",
         "cryptography==3.4.8",
     ],
     extra_require={
